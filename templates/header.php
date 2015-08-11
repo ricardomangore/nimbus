0b1412d7e1783b9bdc9247e68b85fe9fff1bef01
ricardomangore <ricardomangore@gmail.com>
2015-08-11 12:24:08 -0500

    Versión 1.0 Templete header contiene la imagén pricipal del Home del tema y el encabezado principal

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<?php if(is_home() || is_front_page()) : ?>
+<!-- Header -->
+<header id="top" class="header">
+    <div class="text-vertical-center">
+    	<img src="wp-content/uploads/2015/08/logo-intermodalexpress.png" width="30%" height="20%" style="margin-top: 5px;"/>
+    	<!--<img src="http://localhost/wordpress/wp-content/uploads/2015/08/logo-intermodalexpress.png" width="30%" height="20%" style="margin-top: 5px;"/>-->
+        <!--<h1>Start Bootstrap</h1>-->
+        <h3>Piensa Local Actúa Global</h3>
+        <br>
+        <a href="#about" class="btn btn-dark btn-lg">Find Out More</a>
+    </div>
+</header>
+<?php endif; ?>

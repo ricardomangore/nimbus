700bdb1026e7acee91f3bb2a7ad5b441484ebe24
ricardomangore <ricardomangore@gmail.com>
2015-08-11 12:37:49 -0500

    Versión 1.0.1 Se modifica la leyenda del header

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
     	<img src="wp-content/uploads/2015/08/logo-intermodalexpress.png" width="30%" height="20%" style="margin-top: 5px;"/>
     	<!--<img src="http://localhost/wordpress/wp-content/uploads/2015/08/logo-intermodalexpress.png" width="30%" height="20%" style="margin-top: 5px;"/>-->
         <!--<h1>Start Bootstrap</h1>-->
-        <h3>Piensa Local Actúa Global</h3>
+		<br>
         <br>
         <a href="#about" class="btn btn-dark btn-lg">Find Out More</a>
     </div>

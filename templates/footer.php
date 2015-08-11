b55bd3016cb0342500992bc1768b99ac3e5a787d
ricardomangore <ricardomangore@gmail.com>
2015-08-11 12:22:30 -0500

    Versión 1.0 Templete pra el footer wordpress

---
+++ templates/footer.php
@@ -0,0 +1,35 @@
+<!-- Footer -->
+<footer>
+    <div class="container">
+    	    <?php the_content();  ?>
+    </div>
+    <?php wp_footer(); ?>
+</footer><!-- footer -->
+
+
+        <!--<div class="row">
+            <div>
+                <h4><strong>Intermodalexpress</strong>
+                </h4>
+                <p>3481 Melrose Place<br>Beverly Hills, CA 90210</p>
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
+                    <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">name@example.com</a>
+                    </li>
+                </ul>
+                <br>
+                <ul class="list-inline">
+                    <li>
+                        <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-dribbble fa-fw fa-3x"></i></a>
+                    </li>
+                </ul>
+                <hr class="small">
+                <p class="text-muted">Copyright &copy; Your Website 2015</p>
+            </div>
+        </div>-->

de9d8453efb945a96f25cd8b7e61085a4d22f0c3
ricardomangore <ricardomangore@gmail.com>
2015-09-03 11:34:15 -0500

    Actualización de paths absolutos

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
 <!-- Header -->
 <header id="top" class="header">
     <div class="text-vertical-center">
-    	<img src="http://wp.intermodalexpress.com.mx/wp-content/uploads/2015/08/logo-intermodalexpress1.png" width="30%" height="20%" style="margin-top: 5px;"/>
+    	<img src="http://intermodalexpress.com.mx/wp-content/uploads/2015/08/logo-intermodalexpress1.png" width="30%" height="20%" style="margin-top: 5px;"/>
     	<!--<img src="http://localhost/wordpress/wp-content/uploads/2015/08/logo-intermodalexpress.png" width="30%" height="20%" style="margin-top: 5px;"/>-->
         <!--<h1>Start Bootstrap</h1>-->
 		<br>

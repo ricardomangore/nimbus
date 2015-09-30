dd98624f2cbd59e103c5fabc4087dc1c98608f31
ricardomangore <ricardomangore@gmail.com>
2015-09-30 18:28:26 -0500

    se modifico lo ocncerniente al shortcode del cotizador

--- templates/page_quote_result.php
+++ templates/page_quote_result.php
@@ -20,22 +20,34 @@
     $result_array = array();
 	
 	foreach($results as $result){
+		
+		$result_aol = $gestor->get_results("SELECT code, pais, ciudad, aeropuerto FROM aeropuerto WHERE idaeropuerto = $result->aol");
+		
+		$result_aod = $gestor->get_results("SELECT code, pais, ciudad, aeropuerto FROM aeropuerto WHERE idaeropuerto = $result->aod");
+		
 		$result_via = $gestor->get_results("SELECT * FROM via2 
 											LEFT JOIN aeropuerto ON via2.idaeropuerto = aeropuerto.idaeropuerto 
 											WHERE idflete_aereo = $result->idflete_aereo");
 											
-		$result_recargos = $gestor->get_results('SELECT * FROM rel_flete_aereo_recargo_aereo 
+		$result_recargos = $gestor->get_results("SELECT * FROM rel_flete_aereo_recargo_aereo 
 												 LEFT JOIN recargo_aereo ON rel_flete_aereo_recargo_aereo.idrecargo_aereo = recargo_aereo.idrecargo_aereo 
-												 LEFT JOIN aerolinea ON recargo_aereo.idaerolinea = recargo_aereo.idaerolinea 
-												 LEFT JOIN recargo_aereo.idrecargo = recargo.idrecargo');							
+												 LEFT JOIN aerolinea ON recargo_aereo.idaerolinea = aerolinea.idaerolinea
+												 LEFT JOIN recargo ON recargo_aereo.idrecargo = recargo.idrecargo
+												 WHERE idflete_aereo = $result->idflete_aereo");
+												 
+												 						
 		array_push($result_array,array(
 			'flete_aereo' => $result,
 			'via' => $result_via,
-			'recargos' => $result_recargos
+			'recargos' => $result_recargos,
+			'aol' => $result_aol[0],
+			'aod' => $result_aod[0]
 		));
 	}
-	var_dump($result_array);
-
+	
+	var_dump($result_aod);
+	echo "<br>";
+	var_dump($result_aol);
 ?>
 <div class="section">
 	<div class="container">
@@ -45,18 +57,37 @@
 <div class="section">
 	<div class="container">
 		<table class="table">
-			<?php if(empty($results)): ?>
+			<thead>
+				<tr>
+					<th>Origen</th>
+					<th>Destino</th>
+					<th>Precio</th>
+					<th>Detalles</th>
+				</tr>
+			</thead>
+			<?php if(empty($result_array)): ?>
 				<tr><td><h3>No se encontraron fletes ...!!</h3></td></tr>
 			<?php else: ?>
-				<?php foreach($results as $result): ?>
+				<?php foreach($result_array as $result): ?>
 						<tr>
 							<td>
-								<div class='row'>
-									<div class='col-md-2'>Destino: <?php echo $result->pais; ?></div>
-									<div class='col-md-2'>Code: <?php echo $result->code; ?></div>
-									<div class='col-md-2'>Aeropuerto: <?php echo $result->aeropuerto; ?></div>
-									<div class='col-md-2'>Precio: $ <?php echo $result->precio * $param; ?> USD</div>
-								</div>
+								Aeropuerto: <?php echo $result['aol']->aeropuerto; ?><br>
+								Code: <?php echo $result['aol']->code; ?><br>
+								País: <?php echo $result['aol']->pais; ?><br>
+							</td>
+							<td>
+								Aeropuerto: <?php echo $result['aod']->aeropuerto; ?><br>
+								Code: <?php echo $result['aod']->code; ?><br>
+								País: <?php echo $result['aod']->pais; ?><br>
+							</td>
+							<td>
+								$ <?php echo $result['flete_aereo']->precio; ?> USD
+							</td>
+							<td>
+								<?php foreach($result['recargos'] as $recargo): ?>
+									<?php echo $recargo->descripcion . ': $ ' . $recargo->costo;  ?> USD
+									<br>
+								<?php endforeach; ?>
 							</td>
 						</tr>
 				<?php endforeach; ?>

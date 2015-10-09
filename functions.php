e5c6db5d2f3cce837cc5d5dd51875cd2825ca4f1
ricardomangore <ricardomangore@gmail.com>
2015-10-09 12:07:54 -0500

    se elimino la funcio echo $_SERVER

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 define('NIMBUS_ROOT', get_template_directory_uri());
-echo $_SERVER['DOCUMENT_ROOT'];
 include_once ('includes/nimbus_menu.php');
 include_once ('includes/nimbus_short_codes.php');
 include_once ('includes/nimbus_meta_boxes.php');
